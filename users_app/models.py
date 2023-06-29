8bf53a6de9fbde5196e9962312366a6261bd2bf8
Alexandr Peretyatko <keeper_1992@mail.ru>
2023-06-29 16:39:43 +0600

    add api to add new user

--- users_app/models.py
+++ users_app/models.py
@@ -3,6 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    objects = True
     gender = models.CharField(max_length=10, verbose_name='Пол')
     photo = models.ImageField(upload_to='photo/', default='photo/no_photo.jpg')
---
+++ users_app/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework.serializers import ModelSerializer
+from .models import User
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'password', 'first_name', 'last_name', 'gender', 'photo', 'email']
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User(username=validated_data["username"])
+        user.gender = validated_data['gender']
+        user.first_name = validated_data['first_name']
+        user.last_name = validated_data['last_name']
+        user.photo = validated_data['photo']
+        user.set_password(validated_data["password"])
+        user.email = validated_data['email']
+        user.save()
+        return user

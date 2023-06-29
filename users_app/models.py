be837f91b095f199672136ce9adcdd4b545014f9
Alexandr Peretyatko <keeper_1992@mail.ru>
2023-06-29 13:23:36 +0600

    add model User

---
+++ users_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    objects = True
+    gender = models.CharField(max_length=10, verbose_name='Пол')
+    photo = models.ImageField(upload_to='photo/', default='photo/no_photo.jpg')
